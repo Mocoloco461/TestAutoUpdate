7ca55bfa713d3e89423f3442401a0a0b3f4d3a0d
Mocoloco461 <nuhnuh0101@gmail.com>
2024-01-24 20:01:07 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+# Step 0: Define script's current and parent directory paths
+script_path=$(realpath "$0")
+current_directory=$(dirname "$script_path")
+parent_directory=$(dirname "$current_directory")
+
 # Step 1: Rename this script by appending a "1" to its name
-script_name=$(basename -- "$0")
+script_name=$(basename -- "$script_path")
 new_name="${script_name}1"
-mv -- "$0" "$new_name"
+mv -- "$script_path" "$new_name"
 
-# Step 2: Delete all other items in the directory except for the new script
-find . -not -name "$new_name" -delete
+# Moving the script to the parent directory temporarily
+mv -- "$new_name" "$parent_directory"
+cd "$parent_directory"
 
-# Step 3: Clone the GitHub repository
+# Step 2: Delete the original folder containing the script
+# WARNING: This will delete all contents of the script's original directory
+rm -rf "$current_directory"
+
+# Step 3: Clone the GitHub repository into the current location (which is the parent of the original script's folder)
 git clone https://github.com/Mocoloco461/TestAutoUpdate.git
 
 # Step 4: Delete this script file
